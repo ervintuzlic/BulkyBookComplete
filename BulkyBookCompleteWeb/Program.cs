849c6b27fc9964e1f4e6f42aba74995dbbd98bb6
Ervin Tuzlic <ervin@fragnet.net>
2022-08-01 22:56:39 +0200

    Add project files.

---
+++ BulkyBookCompleteWeb/Program.cs
@@ -0,0 +1,50 @@
+//** MVC -> Models Views Controllers
+//** Class in C# is used to describe a model component. Model component coresponds with all data. Properties of that class will be columns in table
+//** If you have 10 tables in database you will have 10 models that coresponds to them
+//** View is all visable part of the website HTML/CSS
+//** Button in website and what happens when you click the button, view will interact with model however view can't interact with model directly
+//** That's why we have controller which acts like an interface between model and view to process all logic
+//** If user uses the button it will send request to Controller which will interact with model (GET DATA) and interact with View (GET PRESENTATION)
+
+//** https:://localhost:55555/Category/Index/3 -> https:://localhost:55555/{Controller}/{Action}/{Id?} Id is optional
+
+//** Models -> Tables in database 
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+//** Add hot reload of the page
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+//** Defined files in wwwroot folder
+app.UseStaticFiles();
+
+app.UseRouting();
+
+//** Always use authentication before authorization of user. Basic fundamentals of authentication and authorization.
+//** Different order in pipeline can break things
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+//** MapControllerRoute that will redirect our request to responding controllers
+app.MapControllerRoute(
+    name: "default",
+    //** Default it would go to Home if there's an action it will go to Index
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
