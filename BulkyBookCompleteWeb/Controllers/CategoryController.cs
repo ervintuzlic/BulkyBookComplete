a626be87d1d010483345b3fc0552c103aee4bbac
Ervin Tuzlic <ervin@fragnet.net>
2022-08-02 12:01:36 +0200

    Retrieve all objects in list from db

--- BulkyBookCompleteWeb/Controllers/CategoryController.cs
+++ BulkyBookCompleteWeb/Controllers/CategoryController.cs
@@ -1,11 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulkyBookCompleteWeb.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyBookCompleteWeb.Controllers
 {
     public class CategoryController : Controller
     {
+
+        //** Create private readonly ApplicationDBContext variable
+        private readonly ApplicationDBContext _db;
+
+        //** Assign db that we got to the _db variable
+        public CategoryController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
+            //** Retrieve all Category objects in list from db and assign it to objCategoryList variable
+            var objCategoryList = _db.Categories.ToList();
             return View();
         }
     }
