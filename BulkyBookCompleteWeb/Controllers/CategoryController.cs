cadcb5e07be7f83cfeee2e303058cea2bde02e72
Ervin Tuzlic <ervin@fragnet.net>
2022-08-02 23:41:21 +0200

    Create view and validation

--- BulkyBookCompleteWeb/Controllers/CategoryController.cs
+++ BulkyBookCompleteWeb/Controllers/CategoryController.cs
@@ -22,5 +22,27 @@ namespace BulkyBookCompleteWeb.Controllers
             IEnumerable<Category> objCategoryList = _db.Categories;
             return View(objCategoryList);
         }
+
+        // GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+        // POST
+        [HttpPost]
+        //** ValidateAntiForgeryToken is there to automatically inject a key and it's validated at start so it helps with Forgery attack
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            //** ModelState.IsValid checks if the model passes all requirements (required, etc)
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                //** RedirectToAction it will find Index inside the same controller -> comma if want to specify other controller
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
     }
 }
