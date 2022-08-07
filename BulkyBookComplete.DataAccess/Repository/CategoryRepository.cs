f17af44db66f6125e84666d59cc5a11617827755
Ervin Tuzlic <ervin@fragnet.net>
2022-08-07 23:03:20 +0200

    CategoryRepository added

---
+++ BulkyBookComplete.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,29 @@
+﻿using BulkyBookComplete.DataAccess.Repository.IRepository;
+using BulkyBookComplete.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBookComplete.DataAccess.Repository
+{
+    internal class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDBContext _db;
+        public CategoryRepository(ApplicationDBContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(Category obj)
+        {
+            _db.Categories.Update(obj);
+        }
+    }
+}
