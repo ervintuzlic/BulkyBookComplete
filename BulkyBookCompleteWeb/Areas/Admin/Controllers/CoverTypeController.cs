5ffe4aae9bf6fb184555f2f9a98f7d9bd700d008
Ervin Tuzlic <ervin@fragnet.net>
2022-08-09 22:19:26 +0200

    CoverType Controller and View, CTOR for Covertype controller

---
+++ BulkyBookCompleteWeb/Areas/Admin/Controllers/CoverTypeController.cs
@@ -0,0 +1,22 @@
+﻿using BulkyBookComplete.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookCompleteWeb.Areas.Admin.Controllers
+{
+    public class CoverTypeController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CoverTypeController(IUnitOfWork obj)
+        {
+            _unitOfWork = obj;
+        }
+
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
