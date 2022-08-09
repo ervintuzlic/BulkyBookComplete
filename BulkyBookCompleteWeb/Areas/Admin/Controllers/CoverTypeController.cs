bc068992306be69662e8018e2ec67f443f664d1d
Ervin Tuzlic <ervin@fragnet.net>
2022-08-09 23:28:37 +0200

    Delete view and CoverTypeController delete added

--- BulkyBookCompleteWeb/Areas/Admin/Controllers/CoverTypeController.cs
+++ BulkyBookCompleteWeb/Areas/Admin/Controllers/CoverTypeController.cs
@@ -75,5 +75,44 @@ namespace BulkyBookCompleteWeb.Controllers
             }
             return View(obj);
         }
+
+        //GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+                TempData["error"] = "Cover Type wasn't found!";
+            }
+
+            var coverTypeFromDb = _unitOfWork.CoverType.GetFirstOrDefault(u => u.Id == id);
+
+            if (coverTypeFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(coverTypeFromDb);
+
+        }
+
+        //POST
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = _unitOfWork.CoverType.GetFirstOrDefault(u => u.Id == id);
+
+            if(obj == null)
+            {
+                TempData["error"] = "Cover Type isn't valid";
+                return RedirectToAction("Index");
+            }
+            _unitOfWork.CoverType.Remove(obj);
+            _unitOfWork.Save();
+            TempData["success"] = "Cover Type has been successfully deleted!";
+            return RedirectToAction("Index");
+
+        }
     }
 }
