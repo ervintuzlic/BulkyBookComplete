a93ab1d3a4be560fb4b53e13e1d540d30b879c74
Ervin Tuzlic <ervin@fragnet.net>
2022-08-09 22:48:20 +0200

    CoverType Index, Layout adjustments, CoverTypeController Create added

--- BulkyBookCompleteWeb/Areas/Admin/Controllers/CoverTypeController.cs
+++ BulkyBookCompleteWeb/Areas/Admin/Controllers/CoverTypeController.cs
@@ -1,8 +1,10 @@
 ﻿using BulkyBookComplete.DataAccess.Repository.IRepository;
+using BulkyBookComplete.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace BulkyBookCompleteWeb.Areas.Admin.Controllers
+namespace BulkyBookCompleteWeb.Controllers
 {
+    [Area("Admin")]
     public class CoverTypeController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
@@ -12,11 +14,32 @@ namespace BulkyBookCompleteWeb.Areas.Admin.Controllers
             _unitOfWork = obj;
         }
 
-
-
         public IActionResult Index()
         {
+            IEnumerable<CoverType> coverTypeList = _unitOfWork.CoverType.GetAll();
+            return View(coverTypeList);
+        }
+
+        //GET
+        public IActionResult Create()
+        {
             return View();
+        }
+
+        //POST 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(CoverType coverType)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.CoverType.Add(coverType);
+                _unitOfWork.Save();
+                TempData["success"] = "Cover Type created successfully!";
+                return RedirectToAction("Index");
+            }
+            TempData["error"] = "Cover Type wasn't created ModelState is invalid!";
+            return View(coverType);
         }
     }
 }
