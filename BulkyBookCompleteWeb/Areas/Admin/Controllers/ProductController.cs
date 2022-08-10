d27d20e44321883368d42f54e9c824a11f2f8be1
Ervin Tuzlic <ervin@fragnet.net>
2022-08-10 10:27:41 +0200

    Changed to Viewmodel

--- BulkyBookCompleteWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyBookCompleteWeb/Areas/Admin/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 ﻿using BulkyBookComplete.DataAccess.Repository.IRepository;
 using BulkyBookComplete.Models;
+using BulkyBookComplete.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Linq;
@@ -25,30 +26,29 @@ namespace BulkyBookCompleteWeb.Controllers
         //GET
         public IActionResult Upsert(int? id)
         {
-            Product product = new();
-
-            // ** Projections using Select
-            IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category.GetAll().Select(
-                u=>new SelectListItem
+            ProductVM productVM = new()
+            {
+                Product = new(),
+                CategoryList = _unitOfWork.Category.GetAll().Select(i => new SelectListItem()
                 {
-                    Text = u.Name,
-                    Value = u.Id.ToString(),
+                    Text = i.Name,
+                    Value = i.Id.ToString()
                 }
-            );
-
-            IEnumerable<SelectListItem> CoverTypeList = _unitOfWork.CoverType.GetAll().Select(
-                u => new SelectListItem
+                ),
+                CoverTypeList = _unitOfWork.CoverType.GetAll().Select(i => new SelectListItem()
                 {
-                    Text = u.Name,
-                    Value = u.Id.ToString(),
+                    Text = i.Name,
+                    Value = i.Id.ToString()
                 }
-            );
+                )
+            };
 
             if (id == null || id == 0)
             {
                 // create product
-                ViewBag.CategoryList = CategoryList;
-                return View(product);
+                //ViewBag.CategoryList = CategoryList;
+                //ViewData["CoverTypeList"] = CoverTypeList;
+                return View(productVM);
 
             }
 
@@ -57,7 +57,7 @@ namespace BulkyBookCompleteWeb.Controllers
                 // update product
             }
 
-            return View(product);
+            return View(productVM);
 
         }
 
